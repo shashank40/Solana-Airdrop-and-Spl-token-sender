d75e2bd1cdc4b86a2dfad23fd664f22b9451ac17
shashank tiwari <shashanktiwari40@gmail.com>
2023-03-27 23:13:33 +0530

    added button

--- src/airdrop_sol/index.ts
+++ src/airdrop_sol/index.ts
@@ -3,7 +3,10 @@ import {showBalance} from '../fetch_balance/index'
 
 export const airdrop = async (address : string, amount: number) => {
     const publicKey = new PublicKey(address);
-    const connection = new Connection("https://api.devnet.solana.com", "confirmed"); 
+    const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
+    if(!connection || connection==null)
+    return `No Account found with this ${address}`
 
     const signature = await connection.requestAirdrop(publicKey, amount*LAMPORTS_PER_SOL);
     const latestBlockHash = await connection.getLatestBlockhash();
