bb5c03390c9fa465976ac0b55b789670d66e3f9c
shashank tiwari <shashanktiwari40@gmail.com>
2023-03-31 01:30:03 +0530

    added path changes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const http = require('http')
-const fs = require('fs')
+import http from 'http'
+import fs from 'fs'
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('./webApp/index.html').pipe(res)
 })
 
+console.log('server started on http://127.0.0.1:3000')
 server.listen(process.env.PORT || 3000)
 
 /// to serve html page with http protocol
